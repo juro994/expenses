769e72b9bfc557fa9fd73fa27b775d118a597e1c
Juraj Jakub <juraj.jakub@gmail.com>
2021-01-09 13:46:53 +0100

    try to fail the test 2

--- src/main/java/sk/juraj/projects/expenses/ExpensesApplication.java
+++ src/main/java/sk/juraj/projects/expenses/ExpensesApplication.java
@@ -1,11 +1,18 @@
 package sk.juraj.projects.expenses;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import sk.juraj.projects.expenses.entity.Category;
+import sk.juraj.projects.expenses.repository.CategoryRepository;
+
 @SpringBootApplication
 public class ExpensesApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensesApplication.class, args);
@@ -13,15 +20,15 @@ public class ExpensesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		var category1 = new Category("Entertainment");
-//		var category2 = new Category("HealthHealthHealthHealthHealthHealthHealthHealthHealthHealthHealth");
-//		var category3 = new Category("Going out");
-//		
-//		categoryRepository.save(category1);
-//		categoryRepository.save(category2);
-//		categoryRepository.save(category3);
-//		
-//		System.out.println(categoryRepository.findAll());
+		var category1 = new Category("Entertainment");
+		var category2 = new Category("Health");
+		var category3 = new Category("Going out");
+		
+		categoryRepository.save(category1);
+		categoryRepository.save(category2);
+		categoryRepository.save(category3);
+		
+		System.out.println(categoryRepository.findAll());
 	}
 
 }
--- src/test/java/sk/juraj/projects/expenses/CategoryIT.java
+++ src/test/java/sk/juraj/projects/expenses/CategoryIT.java
@@ -2,14 +2,28 @@ package sk.juraj.projects.expenses;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import sk.juraj.projects.expenses.controller.CategoryController;
+import sk.juraj.projects.expenses.dto.CategoryDTO;
+import sk.juraj.projects.expenses.entity.Category;
 
 class CategoryIT extends BaseIT {
 
 	@Test
 	public void checkEmptyCategories() throws Exception {
+		var categoryA = new CategoryDTO("Health");
+		var categoryB = new CategoryDTO("Groceries");
+		var categoryC = new CategoryDTO("Home");
+		
+		var categories = List.of(categoryA, categoryB, categoryC);
+		
+		for(var category : categories) {
+			this.restTemplate.postForEntity(getUrlBase(), category, Category.class);
+		}
+		
 		assertThat(this.restTemplate.getForObject(getUrlBase(),
 				String.class)).contains("[]");
 	}
